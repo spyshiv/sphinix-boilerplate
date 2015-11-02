7fe0181e0d9a0d4fac2033ccaf22674d4111126a
Shiv Baran Singh <spyshiv@gmail.com>
2015-11-02 23:11:27 +0530

    Configured Makefile

---
+++ html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index","source/includeme"],titles:["Welcome to document-test&#8217;s documentation!","&lt;no title&gt;"],objects:{},titleterms:{test:0,document:0,welcom:0,indic:0,tabl:0}})
